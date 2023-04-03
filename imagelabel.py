e09bb98ab8213dbdf2bf272b44d10e289b4c1bd3
Sparky1C <116422257+Sparky1C@users.noreply.github.com>
2023-04-03 21:48:48 +1000

    Update imagelabel.py

--- imagelabel.py
+++ imagelabel.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 class Annotator:
     def __init__(self, categories: List[str]):
@@ -13,7 +13,7 @@ class Annotator:
 
 
 class Analyzer:
-    def analyze(self, data: str) -> Dict[str, float]:
+    def analyze(self, data: str, **kwargs) -> Dict[str, float]:
         analysis = {
             "complexity": len(data),
             "readability": len(data.split()),
@@ -23,8 +23,8 @@ class Analyzer:
 
 
 class AnacomAI:
-    def __init__(self):
-        self.logger = self._setup_logger()
+    def __init__(self, logger: Optional[logging.Logger] = None):
+        self.logger = logger or self._setup_logger()
 
     def annotate(self, data: str, categories: List[str]) -> Dict[str, List[str]]:
         """
@@ -39,11 +39,11 @@ class AnacomAI:
         try:
             annotations = annotator.annotate(data)
             return annotations
-        except Exception as e:
+        except (ValueError, TypeError) as e:
             self.logger.exception("Annotating the data failed.")
             raise e
 
-    def analyze(self, data: str) -> Dict[str, float]:
+    def analyze(self, data: str, **kwargs) -> Dict[str, float]:
         """
         Analyzes the given data for complexity, readability, and maintainability.
 
@@ -53,9 +53,9 @@ class AnacomAI:
         analyzer = Analyzer()
 
         try:
-            analysis = analyzer.analyze(data)
+            analysis = analyzer.analyze(data, **kwargs)
             return analysis
-        except Exception as e:
+        except (ValueError, TypeError) as e:
             self.logger.exception("Analyzing the data failed.")
             raise e
 
@@ -75,7 +75,7 @@ class AnacomAI:
 
         return labeled_data
 
-    def analyze_data(self, data: List[str]) -> List[Dict[str, float]]:
+    def analyze_data(self, data: List[str], **kwargs) -> List[Dict[str, float]]:
         """
         Analyzes a large amount of data for complexity, readability, and maintainability.
 
@@ -85,7 +85,7 @@ class AnacomAI:
         analyzed_data = []
 
         for datum in data:
-            analysis = self.analyze(datum)
+            analysis = self.analyze(datum, **kwargs)
             analyzed_data.append(analysis)
 
         return analyzed_data
@@ -103,5 +103,77 @@ class AnacomAI:
         console_handler.setFormatter(console_formatter)
 
         logger.addHandler(console_handler)
+    file_handler = logging.FileHandler('anacomai.log')
+    file_handler.setLevel(logging.ERROR)
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
 
-        return logger
+    logger.addHandler(file_handler)
+
+    return logger
+
+def process_data(self, data: List[str], categories: List[str]) -> List[Dict[str, Union[List[str], float]]]:
+    """
+    Processes a large amount of data by annotating and analyzing it based on the user's input.
+
+    :param data: A list of data strings to process.
+    :param categories: A list of categories to label.
+    :return: A list of dictionaries containing the annotated and analyzed data.
+    """
+    processed_data = []
+
+    for datum in data:
+        annotations = self.annotate(datum, categories=categories)
+        analysis = self.analyze(datum)
+        processed_datum = {"annotations": annotations, "analysis": analysis}
+        processed_data.append(processed_datum)
+
+    return processed_data
+if name == "main":
+anacomai = AnacomAI()
+data = [
+"def calculate_sum(a, b):\n return a + b",
+"class Car:\n def init(self, make, model, year):\n self.make = make\n self.model = model\n self.year = year",
+"import pandas as pd\n\ndata = pd.read_csv('data.csv')\nprint(data.head())"
+]
+categories = ["def", "class", "import"]
+labeled_data = anacomai.label_data(data, categories)
+analyzed_data = anacomai.analyze_data(data)
+processed_data = anacomai.process_data(data, categories)
+
+print(labeled_data)
+print(analyzed_data)
+print(processed_data)
+labeled_data = anacomai.label_data(data, categories)
+analyzed_data = anacomai.analyze_data(data)
+processed_data = anacomai.process_data(data, categories)
+
+print("Labeled Data:")
+print(labeled_data)
+print("")
+
+print("Analyzed Data:")
+print(analyzed_data)
+print("")
+
+print("Processed Data:")
+print(processed_data)
+print("")
+
+# Example of handling an exception
+try:
+    invalid_data = "This is not a valid code snippet"
+    annotations = anacomai.annotate(invalid_data, categories=categories)
+except Exception as e:
+    print("An error occurred:", str(e))
+print("")
+print("")
+
+# Example of handling a warning
+try:
+    warning_data = "This code snippet is too complex"
+    analysis = anacomai.analyze(warning_data)
+except Warning as w:
+    print("A warning occurred:", str(w))
+if name == "main":
+main()
