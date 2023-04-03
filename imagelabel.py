f87e47409630822e748d20b27bd92afe67075fe9
Sparky1C <116422257+Sparky1C@users.noreply.github.com>
2023-04-03 22:01:17 +1000

    Update imagelabel.py

--- imagelabel.py
+++ imagelabel.py
@@ -1,98 +1,99 @@
 import logging
-from typing import Dict, List, Optional
+import numpy as np
 
-class Annotator:
+class ImageAnnotator:
     def __init__(self, categories: List[str]):
         self.categories = categories
 
-    def annotate(self, data: str) -> Dict[str, List[str]]:
+    def annotate(self, data: np.ndarray) -> Dict[str, List[Tuple[int, int]]]:
         annotations = {}
         for category in self.categories:
-            annotations[category] = [f"{category} {i}" for i, word in enumerate(data.split()) if word == category]
+            annotations[category] = [(i, j) for i in range(data.shape[0]) for j in range(data.shape[1]) if np.array_equal(data[i][j], np.array(category))]
         return annotations
 
 
-class Analyzer:
-    def analyze(self, data: str, **kwargs) -> Dict[str, float]:
+class ImageAnalyzer:
+    def analyze(self, data: np.ndarray) -> Dict[str, float]:
         analysis = {
-            "complexity": len(data),
-            "readability": len(data.split()),
-            "maintainability": data.count(";") / max(1, data.count("\n"))
+            "dimensions": (data.shape[0], data.shape[1]),
+            "num_pixels": data.shape[0] * data.shape[1],
+            "average_intensity": np.mean(data),
+            "contrast": np.std(data)
         }
         return analysis
 
 
-class AnacomAI:
-    def __init__(self, logger: Optional[logging.Logger] = None):
-        self.logger = logger or self._setup_logger()
+class ImageProcessor:
+    def __init__(self):
+        self.logger = self._setup_logger()
 
-    def annotate(self, data: str, categories: List[str]) -> Dict[str, List[str]]:
+    def annotate_image(self, data: np.ndarray, categories: List[str]) -> Dict[str, List[Tuple[int, int]]]:
         """
-        Annotates and labels the given data based on the user's input.
+        Annotates and labels the given image based on the user's input.
 
-        :param data: The data to annotate.
+        :param data: The image to annotate.
         :param categories: A list of categories to label.
-        :return: A dictionary containing the annotated and labeled data.
+        :return: A dictionary containing the annotated and labeled image.
         """
-        annotator = Annotator(categories)
+        annotator = ImageAnnotator(categories)
 
         try:
             annotations = annotator.annotate(data)
             return annotations
-        except (ValueError, TypeError) as e:
-            self.logger.exception("Annotating the data failed.")
+        except Exception as e:
+            self.logger.exception("Annotating the image failed.")
             raise e
 
-    def analyze(self, data: str, **kwargs) -> Dict[str, float]:
+    def analyze_image(self, data: np.ndarray) -> Dict[str, float]:
         """
-        Analyzes the given data for complexity, readability, and maintainability.
+        Analyzes the given image for dimensions, number of pixels, average intensity, and contrast.
 
-        :param data: The data to analyze.
+        :param data: The image to analyze.
         :return: A dictionary containing the analysis results.
         """
-        analyzer = Analyzer()
+        analyzer = ImageAnalyzer()
 
         try:
-            analysis = analyzer.analyze(data, **kwargs)
+            analysis = analyzer.analyze(data)
             return analysis
-        except (ValueError, TypeError) as e:
-            self.logger.exception("Analyzing the data failed.")
+        except Exception as e:
+            self.logger.exception("Analyzing the image failed.")
             raise e
 
-    def label_data(self, data: List[str], categories: List[str]) -> List[Dict[str, List[str]]]:
+    def label_images(self, data: List[np.ndarray], categories: List[str]) -> List[Dict[str, List[Tuple[int, int]]]]:
         """
-        Annotates and labels a large amount of data based on the user's input.
+        Annotates and labels a large amount of images based on the user's input.
 
-        :param data: A list of data strings to annotate.
+        :param data: A list of images to annotate.
         :param categories: A list of categories to label.
-        :return: A list of dictionaries containing the annotated and labeled data.
+        :return: A list of dictionaries containing the annotated and labeled images.
         """
         labeled_data = []
 
         for datum in data:
-            annotations = self.annotate(datum, categories=categories)
+            annotations = self.annotate_image(datum, categories=categories)
             labeled_data.append(annotations)
 
         return labeled_data
 
-    def analyze_data(self, data: List[str], **kwargs) -> List[Dict[str, float]]:
+    def analyze_images(self, data: List[np.ndarray]) -> List[Dict[str, float]]:
         """
-        Analyzes a large amount of data for complexity, readability, and maintainability.
+        Analyzes a large amount of images for dimensions, number of pixels, average intensity, and contrast.
 
-        :param data: A list of data strings to analyze.
+        :param data: A list of images to analyze.
         :return: A list of dictionaries containing the analysis results.
         """
         analyzed_data = []
 
         for datum in data:
-            analysis = self.analyze(datum, **kwargs)
+            analysis = self.analyze_image(datum)
             analyzed_data.append(analysis)
 
         return analyzed_data
 
     def _setup_logger(self):
         """
-        Sets up a logger for the AnacomAI class.
+        Sets up a logger for the ImageProcessor class.
         """
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
@@ -103,77 +104,5 @@ class AnacomAI:
         console_handler.setFormatter(console_formatter)
 
         logger.addHandler(console_handler)
-    file_handler = logging.FileHandler('anacomai.log')
-    file_handler.setLevel(logging.ERROR)
-    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(file_formatter)
 
-    logger.addHandler(file_handler)
-
-    return logger
-
-def process_data(self, data: List[str], categories: List[str]) -> List[Dict[str, Union[List[str], float]]]:
-    """
-    Processes a large amount of data by annotating and analyzing it based on the user's input.
-
-    :param data: A list of data strings to process.
-    :param categories: A list of categories to label.
-    :return: A list of dictionaries containing the annotated and analyzed data.
-    """
-    processed_data = []
-
-    for datum in data:
-        annotations = self.annotate(datum, categories=categories)
-        analysis = self.analyze(datum)
-        processed_datum = {"annotations": annotations, "analysis": analysis}
-        processed_data.append(processed_datum)
-
-    return processed_data
-if name == "main":
-anacomai = AnacomAI()
-data = [
-"def calculate_sum(a, b):\n return a + b",
-"class Car:\n def init(self, make, model, year):\n self.make = make\n self.model = model\n self.year = year",
-"import pandas as pd\n\ndata = pd.read_csv('data.csv')\nprint(data.head())"
-]
-categories = ["def", "class", "import"]
-labeled_data = anacomai.label_data(data, categories)
-analyzed_data = anacomai.analyze_data(data)
-processed_data = anacomai.process_data(data, categories)
-
-print(labeled_data)
-print(analyzed_data)
-print(processed_data)
-labeled_data = anacomai.label_data(data, categories)
-analyzed_data = anacomai.analyze_data(data)
-processed_data = anacomai.process_data(data, categories)
-
-print("Labeled Data:")
-print(labeled_data)
-print("")
-
-print("Analyzed Data:")
-print(analyzed_data)
-print("")
-
-print("Processed Data:")
-print(processed_data)
-print("")
-
-# Example of handling an exception
-try:
-    invalid_data = "This is not a valid code snippet"
-    annotations = anacomai.annotate(invalid_data, categories=categories)
-except Exception as e:
-    print("An error occurred:", str(e))
-print("")
-print("")
-
-# Example of handling a warning
-try:
-    warning_data = "This code snippet is too complex"
-    analysis = anacomai.analyze(warning_data)
-except Warning as w:
-    print("A warning occurred:", str(w))
-if name == "main":
-main()
+        return logger
